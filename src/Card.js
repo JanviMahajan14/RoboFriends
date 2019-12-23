4ae7f2079bd87833e05654572bf1e2ae0b57e79e
JanviMahajan14 <janvimahajan337@gmail.com>
2019-12-23 20:37:19 +0530

    Resolved

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+
+    return (
+        <div className="tc bg-light-blue dib br3 pd3 mr8 grow shadow-5 ">
+            <img src='https://robohash.org/text' alt="photo" width="200px" height="200px" />
+            <h2 className="f3">Sammy Sam</h2>
+            <p> Email:18bcs052@smvdu.ac.in</p>
+        </div>
+    );
+}
+
+export default Card;
